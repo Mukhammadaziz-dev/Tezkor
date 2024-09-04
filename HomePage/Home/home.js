ad451b4ae281f0e97f8380241a3b82cbda4aa3d9
BMukhammadaziz <MukhammadazizBakhromov@gmail.com>
2024-09-04 19:58:01 +0500

    new

--- HomePage/Home/home.js
+++ HomePage/Home/home.js
@@ -24,24 +24,25 @@ setInterval(() => {
 
 
 fetch("https://66ab5539636a4840d7ca3261.mockapi.io/dcd/products")
-            .then(response => response.json())
-            .then(data => {
-                let alement = document.querySelector("#alements");
-                let template = document.querySelector(".product-box");
+    .then(response => response.json())
+    .then(data => {
+        let alement = document.querySelector("#alements");
+        let template = document.querySelector(".product-box");
 
-                let housewares = data.filter(item => item.type === "housewares");
+        let filteredItems = data.filter(item => 
+            ["housewares", "electronics", "accessory", "clothes"].includes(item.type)
+        );
 
-                housewares.forEach(item => {
-                    let clone = template.cloneNode(true);
-                    clone.style.display = "block";
+        filteredItems.forEach(item => {
+            let clone = template.cloneNode(true);
+            clone.style.display = "block";
 
-                    clone.querySelector(".product-image").src = item.img;
-                    clone.querySelector(".product-image").alt = item.name;
-                    clone.querySelector(".product-name").textContent = item.name;
-                    clone.querySelector(".product-desc").textContent = item.desc;
-                    clone.querySelector(".product-price").textContent = `$${item.price}`;
+            clone.querySelector(".product-image").src = item.img;
+            clone.querySelector(".product-image").alt = item.name;
+            clone.querySelector(".product-name").textContent = item.name;
+            clone.querySelector(".product-price").textContent = `$${item.price}`;
 
-                    alement.appendChild(clone);
-                });
-            })
-            .catch(error => console.error('Error fetching data:', error));
+            alement.appendChild(clone);
+        });
+    })
+    .catch(error => console.error('Error fetching data:', error));
