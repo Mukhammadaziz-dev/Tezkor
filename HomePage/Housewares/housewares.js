3a3d91bea978fcb9f768d0dbad990e79b9f4094f
khusanovumarbek <umarkhusanovv@gmail.com>
2024-09-06 00:20:13 +0500

    housewares

---
+++ HomePage/Housewares/housewares.js
@@ -0,0 +1,30 @@
+fetch("https://66ab5539636a4840d7ca3261.mockapi.io/dcd/products")
+    .then(response => response.json())
+    .then(data => {
+        let umar = document.querySelector(".container")
+        
+
+
+        let housew = data.filter(item => item.type === "housewares");
+
+
+console.log(housew);
+
+        housew.map(item => {
+
+
+            umar.innerHTML += `
+            <div class="box">
+            <a href="#">   <img src="${item.img}" alt="${item.title}" class="product-image" > </a>
+                    <h2 class="product-name">${item.name}</h2>
+                    <p class="product-desc">${item.desc}</p>
+                    <p class="product-price">$${item.price}</p>
+            </div>
+            `;
+
+           
+        });
+    })
+    .catch(error => console.error('Error fetching data:', error));
+
+    
